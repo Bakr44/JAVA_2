dde1a8f0189e33488a047cd5ac40b4a473fb99dc
Bakr Hawsawi <bakrhawsawi4@gmail.com>
2023-08-01 22:42:45 +0300

    add more in main class

--- JAVA_2/assignment--8/src/Main.java
+++ JAVA_2/assignment--8/src/Main.java
@@ -25,6 +25,12 @@ public class Main {
         System.out.println("Circle perimeter: " +c2.getPerimeter());
         System.out.println(c2.toString());
         System.out.println(" ");
+        Circle c3=new Circle(65.0);
+        System.out.println("Circle area: " + c3.getArea());
+        System.out.println("Circle perimeter: " +c3.getPerimeter());
+        System.out.println(c3.toString());
+        System.out.println(" ");
+
 
         Rectangle r1=new Rectangle("Yellow",false,3,5);
         System.out.println("Rectangle area wideth=3 ,length=5: " +r1.getArea());
@@ -35,6 +41,13 @@ public class Main {
         System.out.println("Rectangle area wideth=20 ,length=10: " +r2.getArea());
         System.out.println("Rectangle perimeter: " +r2.getPerimeter());
         System.out.println(r2.toString());
+        System.out.println(" ");
+        Rectangle r3=new Rectangle();
+        r3.setLength(90.0);
+        r3.setWidth(45.0);
+        System.out.println("Rectangle area wideth=45 ,length=90: " +r3.getArea());
+        System.out.println("Rectangle perimeter: " +r3.getPerimeter());
+        System.out.println(r3.toString());
         System.out.println(" ");
 
         Square sq1=new Square(2.0);
@@ -48,5 +61,11 @@ public class Main {
         System.out.println("Square area side=40: " +sq2.getArea());
         System.out.println("Square perimeter: " +sq2.getPerimeter());
         System.out.println(sq2.toString());
+        System.out.println(" ");
+        Square sq3=new Square(10,"Black",true);
+        System.out.println("Square area side=10: " +sq3.getArea());
+        System.out.println("Square perimeter: " +sq3.getPerimeter());
+        System.out.println(sq3.toString());
+
     }
 }
